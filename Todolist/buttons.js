e8b7bc7c8bf06512bd11df0d211556eaea3f6af3
Bohdan Lysov <BogUkraine@gmail.com>
2018-10-08 20:36:32 +0300

    Test

--- Todolist/buttons.js
+++ Todolist/buttons.js
@@ -1,7 +1,7 @@
 const hide_but = document.querySelector(".hide_button");
 const show_but = document.querySelector(".show_button");
 
-const show_button = show_but.addEventListener('click', () => {
+export const show_button = () => { show_but.addEventListener('click', () => {
     for(let i = 1; i < ul_checked.childNodes.length; i++){
         ul_checked.childNodes[i].style.visibility = 'visible';
     }
@@ -9,14 +9,14 @@ const show_button = show_but.addEventListener('click', () => {
     show_but.style.visibility = 'hidden';
     hide_but.style.visibility = 'visible';
 }, false);
+}
 
-const hide_button = hide_but.addEventListener('click', () => {
+export const hide_button = () => { hide_but.addEventListener('click', () => {
     for(let i = 1; i < ul_checked.childNodes.length; i++){
         ul_checked.childNodes[i].style.visibility = 'hidden';
     }
-    bool_hid_button = false;
+    bool_hid_button = false; 
     show_but.style.visibility = 'visible';
     hide_but.style.visibility = 'hidden';
 }, false);
-
-export {hide_button, show_button};
+}
--- Todolist/index.js
+++ Todolist/index.js
@@ -1,6 +1,7 @@
 import {add_button} from "./button_add.js";
 import {hide_button, show_button}  from "./buttons.js";
-
+import {createListItem} from "./list.js"
+/*
 var array = [
     "This is a list item",
     "Another list item",
@@ -8,13 +9,15 @@ var array = [
     "Another list item2",
     "This is blablabla"
 ];
-
+*/
 window.addEventListener('load', () => {
     add_button();
     hide_button();
     show_button();
 
+    /*
     for(let i = 0; i < array.length; i++){
         createListItem(array[i]);
     }
+    */
 });
